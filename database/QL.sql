1f39fe0bc71efe8a9c7546d89c69d2940e6d46da
Cherchuzo <58659365+Cherchuzo@users.noreply.github.com>
2021-05-04 19:24:41 +0200

    SQL UPDATE (QL ADDED)

---
+++ database/QL.sql
@@ -0,0 +1,15 @@
+INSERT INTO utente VALUES($nickname,$password,$email,$naz,$dataNascita);
+
+SELECT utente.Nickname FROM utente
+WHERE utente.Nickname = $nickname AND utente.Password = $password;
+
+SELECT * FROM utente 
+WHERE utente.Nickname = $nickname AND utente.Email = $email;
+
+SELECT * FROM utente
+WHERE utente.Nickname = $nickname AND utente.Password = $password;
+
+UPDATE utente SET utente.Nickname = $nickname WHERE utente.Nickname = $oldNickname;
+UPDATE utente SET utente.Email = $email WHERE utente.Nickname = $nickname;
+UPDATE utente SET utente.Password = $password WHERE utente.Nickname = $nickname;
+UPDATE utente SET utente.Nazione = $naz WHERE utente.Nickname = $nickname;
--- database/database.sql
+++ database/database.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS ApeOpsArcade;
 CREATE DATABASE ApeOpsArcade;
 
 USE ApeOpsArcade;
